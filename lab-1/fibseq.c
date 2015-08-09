70a10dbb103c135ab26e5b977458dc8145137b18
FizzYe <4a2g0039@stust.edu.tw>
2015-08-09 15:51:58 +0800

    Update fibseq.c

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-   int fibonacci(int number)
-     {
-        int answer;
-        if(number == 0 || number== 1)
-          ans = 1;
-        else      
-            answer = fibonacci(n-1)+fibonacci(n-2);
-        return(answer);
-     }
-  printf("The fibonacci sequence at %d is: %d\n", number, answer);
+int fibonacci(int number)
+{
+   int answer;
+   if(number == 0 || number== 1)
+      answer = 1;
+   else      
+      answer = fibonacci(n-1)+fibonacci(n-2);
+   return(answer);
+  printf("The fibonacci sequence at %d is: %d\n",number,answer);
 }
-
